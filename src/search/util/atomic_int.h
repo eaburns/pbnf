b3ab1ea43779ddbbbf3ae962ed7ea47a4deeaeaf
Ethan Burns <eaburns@unh.edu>
2008-10-14 09:54:48 -0400

    Add atomic swap and an atomic compare and swap operation.

--- src/search/util/atomic_int.h
+++ src/search/util/atomic_int.h
@@ -25,19 +25,19 @@
  */
 class AtomicInt {
 public:
-	AtomicInt(int val) { value = val; }
+	AtomicInt(unsigned int val) { value = val; }
 
-	inline int read(void) const { return value; }
+	inline unsigned int read(void) const { return value; }
 
-	inline void set(int i) { value = i; }
+	inline void set(unsigned int i) { value = i; }
 
-	inline void add(int i) {
+	inline void add(unsigned int i) {
 		__asm__ __volatile__(LOCK_PREFIX "addl %1,%0"
 				     :"=m"(value)
 				     :"ir"(i), "m"(value));
 	}
 
-	inline void sub(int i) {
+	inline void sub(unsigned int i) {
 		__asm__ __volatile__(LOCK_PREFIX "subl %1,%0"
 				     :"=m"(value)
 				     :"ir"(i), "m"(value));
@@ -55,8 +55,40 @@ public:
 				     :"m"(value));
 	}
 
+	inline unsigned int swap(unsigned int n) {
+		__asm__ __volatile__("xchgl %0,%1"
+				     : "=r" (n)
+				     : "m" (value), "0" (n)
+				     : "memory");
+
+		return n;
+	}
+
+	/**
+	 * Atomic compare and swap.  This operation takes an old value
+	 * and a new value.  If the AtomicInt currently contains the
+	 * old value passed as a parameter here, the new value is
+	 * atomically swapped in.  If the AtomicInt contains a value
+	 * that is not the old value given as a parameter, the new
+	 * value is *not* swapped in.
+	 * \param o The expected old value.
+	 * \param n The new value to swap if the current value is 'o'.
+	 * \return The value before this operation.  If the return
+	 *         value is equivilant to 'o', then the operation was
+	 *         a success, otherwise it was not.
+	 */
+	inline unsigned int cmp_and_swap(unsigned int o, unsigned int n) {
+		unsigned int prev;
+
+		__asm__ __volatile__("cmpxchgl %1,%2"
+				     : "=a"(prev)
+				     : "r"(n), "m"(value), "0"(o)
+				     : "memory");
+		return prev;
+	}
+
 private:
-	volatile int value;
+	volatile unsigned int value;
 };
 
 #endif	/* !_ATOMIC_INT_H_ */
