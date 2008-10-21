cf91f4f272e6bc284d3c84ee856764bb4a91e1ca
Ethan Burns <eaburns@unh.edu>
2008-10-21 13:00:55 -0400

    Add some nice comments to thread_test.cc.

--- src/search/thread_test.cc
+++ src/search/thread_test.cc
@@ -2,7 +2,7 @@
 /**
  * \file thread_test.cc
  *
- *
+ * Test the Thread class.
  *
  * \author Ethan Burns
  * \date 2008-10-21
@@ -14,16 +14,30 @@
 
 using namespace std;
 
+/**
+ * An example Thread class.  This class takes two numbers, a unique ID
+ * value and a number to count till.  When this thread is started, it
+ * counts from 0 to num-1 and prints each number it counts along with
+ * its given id value and its pthread thread_id value.
+ */
 class TestThread : public Thread {
 public:
+	/**
+	 * Create a new test thread class with the given ID and number
+	 * to count to.
+	 */
 	TestThread(int id, int num) : id(id), num(num) {}
 
 	~TestThread() {}
 
+	/**
+	 * The run function actually does the work of the thread.
+	 */
 	virtual void run(void) {
-		for (int i = 0; i < num; i += 1)
+		for (int i = 0; i < num; i += 1) {
 			cout << "thread " << get_id()
 			     << "(" << id << "): " << i << endl;
+		}
 	}
 
 private:
@@ -40,10 +54,10 @@ int main(void)
 	one.start();
 	two.start();
 
-	one.join();
+	one.join();		// wait for thread `one' to complete
 	cout << "one finished" << endl;
 
-	two.join();
+	two.join();		// wait for thread `two' to complete
 	cout << "two finished" << endl;
 
 	return 0;
