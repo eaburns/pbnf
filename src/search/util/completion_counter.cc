766c976728a268a49c4ae1c5d87fb26554e4aa20
Ethan Burns <eaburns@unh.edu>
2008-10-24 18:30:05 -0400

    Add a CompletionCounter class.

---
+++ src/search/util/completion_counter.cc
@@ -0,0 +1,39 @@
+/* -*- mode:linux -*- */
+/**
+ * \file completion_counter.cc
+ *
+ *
+ *
+ * \author Ethan Burns
+ * \date 2008-10-24
+ */
+
+#include <pthread.h>
+
+#include "completion_counter.h"
+
+CompletionCounter::CompletionCounter(unsigned int max)
+	: counter(0), max(max)
+{
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&cond, NULL);
+}
+
+void CompletionCounter::complete(void)
+{
+	pthread_mutex_lock(&mutex);
+	counter += 1;
+	pthread_cond_signal(&cond);
+	pthread_mutex_unlock(&mutex);
+}
+
+/**
+ * Wait for all the completions. (counter == max)
+ */
+void CompletionCounter::wait(void)
+{
+	pthread_mutex_lock(&mutex);
+	while (counter < max)
+		pthread_cond_wait(&cond, &mutex);
+	pthread_mutex_unlock(&mutex);
+}
---
+++ src/search/util/completion_counter.h
@@ -0,0 +1,30 @@
+/* -*- mode:linux -*- */
+/**
+ * \file completion_counter.h
+ *
+ *
+ *
+ * \author Ethan Burns
+ * \date 2008-10-24
+ */
+
+#if !defined(_COMPLETION_COUNTER_H_)
+#define _COMPLETION_COUNTER_H_
+
+#include <pthread.h>
+
+class CompletionCounter {
+public:
+	CompletionCounter(unsigned int max);
+
+	void complete(void);
+	void wait(void);
+
+private:
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+	unsigned int counter;
+	unsigned int max;
+};
+
+#endif	/* !_COMPLETION_COUNTER_H_ */
