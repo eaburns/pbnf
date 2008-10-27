2ca3d5269b5332c401f54bdf794138307eeb453e
Ethan Burns <eaburns@unh.edu>
2008-10-27 14:59:12 -0400

    Make the PSDD run script cooler.

--- src/search/run_psdd_vary_ratio.sh
+++ src/search/run_psdd_vary_ratio.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-LIMIT=20
 threads=$1
+board="grid/jordan_unit_four-way_500x500_2"
 
 echo -e "# PSDD With $threads threads"
-echo -e "# nblocks/thread\t\ttime (seconds)"
-for ((i=1; i < LIMIT; i++))
+echo -e "# Board: $board"
+echo -e "# weight\tnblocks/thread\t\ttime (seconds)"
+for ((j=1; j < 6; j++))
 do
-    printf "  %2d\t\t\t\t" $i
-    (/usr/bin/time -f%ereal ./search psdd-$threads-$i \
-	< grid/jordan_unit_four-way_1200x2000_1) 2>&1 \
-	| grep "real" | awk -Fr '{ print $1 }'
+    for ((i=1; i < 10; i++))
+    do
+	printf "  1.%d\t\t%2d\t\t\t" $j $i
+	(/usr/bin/time -f%ereal ./search dynpsdd-$threads-$i-1.$j \
+	    < $board) 2>&1 | grep "real" | awk -Fr '{ print $1 }'
+    done
 done
