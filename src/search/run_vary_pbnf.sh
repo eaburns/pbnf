8ca080e6e5588aa4a99541c56afbc82faf7d6722
Ethan Burns <eaburns@unh.edu>
2008-11-14 11:12:41 -0500

    Update run_vary_pdnf.sh

--- src/search/run_vary_pbnf.sh
+++ src/search/run_vary_pbnf.sh
@@ -5,8 +5,9 @@ do
     for min in 1 10 20 50
     do
 	for nblocks in 1 5 10 15 20
+#	for nblocks in 2 4 6 8 12
 	do
-	    for (( threads=1; threads <= 10; threads++ ))
+	    for (( threads=1; threads <= 15; threads++ ))
 	    do
 		./run_grids.sh ${alg}pbnf \
 		    -m $min \
