f5959bff0f7049ac4e2069fbef5d673347d72e18
Ethan Burns <eaburns@unh.edu>
2008-11-20 15:20:09 -0500

    Add a new run_vary_bfpsdd.sh script.

--- src/search/run_vary.sh
+++ src/search/run_vary.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+./run_vary_bfpsdd.sh
+./run_vary_safepbnf.sh
+./run_vary_pbnf.sh
 ./run_vary_dynpsdd.sh
+./run_vary_psdd.sh
 ./run_vary_kbfs.sh
 ./run_vary_multiastar
 ./run_vary_pastar.sh
-./run_vary_pbnf.sh
-./run_vary_psdd.sh
-./run_vary_safepbnf.sh
---
+++ src/search/run_vary_bfpsdd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for nblocks in 1 2 4 8 10
+do
+    for (( threads=1; threads <= 10; threads++ ))
+    do
+	./run_grids.sh bfpsdd \
+	    -t $threads \
+	    -n $nblocks
+	done
+done
