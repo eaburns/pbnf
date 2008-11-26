e8c3fa287efa6a940bf63359f8e727c00f0a8cb3
Ethan Burns <eaburns@unh.edu>
2008-11-26 10:51:18 -0500

    Add tests to make sure our heuristic was admissible.

--- src/search/tiles_search.cc
+++ src/search/tiles_search.cc
@@ -10,6 +10,7 @@
 
 #define _POSIX_C_SOURCE 200112L
 
+#include <assert.h>
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
@@ -57,6 +58,18 @@ int main(int argc, char *argv[])
 	if (path) {
 		cout << "cost: " << (int) path->at(0)->get_g() << endl;
 		cout << "length: " << path->size() << endl;
+
+		// Make sure that the heuristic was actually admissible!
+		for (unsigned int i = path->size() - 1; i >= 0; i -= 1) {
+			const State *s = path->at(i);
+			unsigned int togo = path->at(0)->get_g() - s->get_g();
+			assert(s->get_h() <= togo);
+			if (i > 0)
+				assert(s->get_h() > 0);
+			if (i == 0)
+				break;
+		}
+
 		for (unsigned int i = 0; i < path->size(); i += 1)
 			delete path->at(i);
 		delete path;
