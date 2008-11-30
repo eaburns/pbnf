d07eb4b086fa6fcbfa5f52bbcbdbc9331c730a8b
Ethan Burns <eaburns@unh.edu>
2008-11-29 21:15:31 -0500

    Fix a syntax error.

--- src/search/scripts/run_tiles_vary_pbnf.sh
+++ src/search/scripts/run_tiles_vary_pbnf.sh
@@ -3,6 +3,7 @@
 for min in 5 10 30 60 80
 do
     for (( threads=1; threads <= 10; threads++ ))
-    ./scripts/run_tiles.sh pbnf -m $min -t $threads $@
+    do
+        ./scripts/run_tiles.sh pbnf -m $min -t $threads $@
     done
 done
