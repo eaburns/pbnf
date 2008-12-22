4f4fcf4969d4e233024bd0d9397facc0906db49e
Ethan Burns <eaburns@unh.edu>
2008-12-22 18:37:54 -0500

    Initialize the fields of the sigaction structure.

--- src/search/util/timeout.cc
+++ src/search/util/timeout.cc
@@ -33,7 +33,11 @@ void timeout(unsigned int sec)
 {
 	struct sigaction sa;
 
+	sa.sa_flags = 0;
+	sa.sa_sigaction = NULL;
+	sa.sa_restorer = NULL;
 	sa.sa_handler = alarm_action;
+	sigfillset(&sa.sa_mask);
 	sigaction(SIGALRM, &sa, NULL);
 
 	alarm(sec);
