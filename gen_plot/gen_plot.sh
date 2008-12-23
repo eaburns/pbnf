718fd718ba00e02db8cdf27cafea2a12a76c50f3
Ethan Burns <eaburns@unh.edu>
2008-12-23 11:48:37 -0500

    Add a gen_plot.sh script to ease the generation of plots.

---
+++ gen_plot/gen_plot.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+GEN_PLOT=/home/rai/eaburns/src/ocaml/ps_plot/gen_plot.unix_unknown
+
+# Some y keys.
+#
+# -ykey wall_ttime -ylabel time-seconds
+# -ykey raw_exp -ylabel total-expansions
+
+
+
+#
+# Grid World
+#
+if $(`which false`)
+then
+MOVES=Four-way
+COSTS=Unit
+WIDTH=2000
+HEIGHT=1200
+PROB=25
+$GEN_PLOT \
+    -root /home/rai/eaburns/data/grid.old type=run \
+    obstacles=uniform \
+    moves=$MOVES \
+    costs=$COSTS \
+    width=$WIDTH \
+    height=$HEIGHT \
+    prob=0.$PROB \
+    -xkey threads \
+    -ykey wall_ttime -ylabel time-seconds \
+    -title time-$COSTS-$MOVES-$WIDTH-$HEIGHT-0$PROB \
+    -astar \
+    -namedalg safepbnf safepbnf-80min-625nblocks "min-expansions=80 nblocks=625" \
+    -namedalg bfpsdd bfpsdd-10nblocks "nblocks=100" \
+    -alg kbfs ""
+fi
+
+
+#
+# Sliding Tiles
+#
+if $(`which true`)
+then
+COLS=4
+ROWS=3
+$GEN_PLOT \
+    -root /home/rai/eaburns/data/tiles type=run \
+    model=random \
+    cols=$COLS \
+    rows=$ROWS \
+    -xkey threads \
+    -ykey wall_ttime -ylabel time-seconds \
+    -title time-$COLS-$ROWS \
+    -namedalg safepbnf safepbnf-80min "min-expansions=80" \
+    -alg bfpsdd ""
+fi
