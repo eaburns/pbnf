782e0fd9e619642848dc1f76748245b05648dcbb
Ethan Burns <eaburns@unh.edu>
2008-12-23 14:29:01 -0500

    Add the beginning of the abstract state merging code.

---
+++ src/search/div_merge_project.cc
@@ -0,0 +1,40 @@
+/* -*- mode:linux -*- */
+/**
+ * \file div_merge_project.cc
+ *
+ *
+ *
+ * \author Ethan Burns
+ * \date 2008-12-19
+ */
+
+#include <vector>
+
+#include "div_merge_project.h"
+
+using namespace std;
+
+DivMergeProject::DivMergeProject(unsigned int div, Projeciton *p)
+	: div(div), projection(p) {}
+
+unsigned int DivMergeProject::project(const State *s)
+{
+	return projection->project(s) / div;
+}
+
+unsigned int DivMergeProject::get_num_nblocks(void)
+{
+	return projection->get_num_nblocks() / div;
+}
+
+vector<unsigned int> DivMergeProject::get_successors(unsigned int b)
+{
+	set<unsigned int> s;
+
+}
+
+vector<unsigned int> DivMergeProject::get_predecessors(unsigned int b)
+{
+	set<unsigned int> s;
+
+}
---
+++ src/search/div_merge_project.h
@@ -0,0 +1,41 @@
+/* -*- mode:linux -*- */
+/**
+ * \file div_merge_project.h
+ *
+ *
+ *
+ * \author Ethan Burns
+ * \date 2008-12-19
+ */
+
+#if !defined(_DIV_MERGE_PROJECT_H_)
+#define _DIV_MERGE_PROJECT_H_
+
+#include <vector>
+
+#include "projection.h"
+
+class State;
+
+using namespace std;
+
+/**
+ * An abstract projection function class.
+ */
+class DivMergeProject : public Projection {
+public:
+	DivMergeProject(unsigned int div, Projection *p);
+
+	unsigned int project(const State *s);
+
+	unsigned int get_num_nblocks(void);
+
+	vector<unsigned int> get_successors(unsigned int b);
+
+	vector<unsigned int> get_predecessors(unsigned int b);
+private:
+	unsigned int div;
+	Projection *projection;
+};
+
+#endif	/* !_DIV_MERGE_PROJECT_H_ */
