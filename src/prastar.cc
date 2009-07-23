14e931906e68c7125c47cbf8eb15b1d4dbe9d467
Ethan Burns <eaburns@unh.edu>
2009-07-23 10:26:49 -0400

    Fix a typeo in PRA* output.

--- src/prastar.cc
+++ src/prastar.cc
@@ -354,7 +354,8 @@ void PRAStar::output_stats(void)
 	cout << "average-time-acquiring-locks: "
 	     << Mutex::get_avg_lock_acquisition_time() << endl;
 	cout << "total-time-waiting: " << time_spinning << endl;
-	cout << "avg-time-waiting: " << time_spinning / n_threads << endl;
+	cout << "avgerage-time-waiting: "
+	     << time_spinning / n_threads << endl;
 	cout << "average-open-size: " << avg_open_size << endl;
 	cout << "max-open-size: " << max_open_size << endl;
 }
