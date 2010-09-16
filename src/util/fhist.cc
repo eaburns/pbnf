0f8e2f5e36bf7ff4302a514f615b2bd3ac50508f
Ethan Burns <burns.ethan@gmail.com>
2010-09-16 07:21:35 -0400

    Add the fhist.{cc,h} files.

---
+++ src/util/fhist.cc
@@ -0,0 +1,39 @@
+/**
+ * \file fhist.cc
+ *
+ * A histogram of f values.
+ *
+ * \author eaburns
+ * \date 08-09-2010
+ */
+
+#include "fhist.h"
+#include "mutex.h"
+#include "fixed_point.h"
+
+#include <iostream>
+#include <vector>
+
+void F_hist::see_f(fp_type f)
+{
+	unsigned long fint = (unsigned long) DOUBLE(f);
+
+	if (Thread::current())
+		mutex.lock();
+	if (counts.size() < fint + 1)
+		counts.resize(fint + 1);
+
+	counts[fint] += 1;
+	if (Thread::current())
+		mutex.unlock();
+}
+
+void F_hist::output_above(std::ostream &o, fp_type opt)
+{
+	for (unsigned int i = 0; i < counts.size(); i += 1) {
+		if (counts[i] > 0) {
+			o << (i / (double) DOUBLE(opt)) << '\t'
+			  << counts[i] << std::endl;
+		}
+	}
+}
---
+++ src/util/fhist.h
@@ -0,0 +1,32 @@
+/**
+ * \file fhist.h
+ *
+ * A simple histogram of integer valued f values.
+ *
+ * \author eaburns
+ * \date 08-09-2010
+ */
+
+#include "fixed_point.h"
+#include "mutex.h"
+
+#include <fstream>
+#include <vector>
+
+class F_hist {
+public:
+	/*
+	 * Add a count of 1 to the histogram for 'f'
+	 */
+	void see_f(fp_type f);
+
+	/*
+	 * Output counts for all values above 'opt' to 'o'.
+	 */
+	void output_above(std::ostream &o, fp_type opt);
+
+private:
+	std::vector<unsigned long> counts;
+
+	Mutex mutex;
+};
